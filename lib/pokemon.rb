6ef21f03e21298bef7d0499cc065f24d52fe74db
Helen Du <helendu.614@gmail.com>
2018-05-18 02:13:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,7 @@ class Pokemon
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
-  
+
   def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
     @name = name
@@ -23,5 +23,5 @@ class Pokemon
   def alter_hp(new_hp, db)
     db. execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
-  
+
 end
