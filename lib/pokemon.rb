af5c42774b8976ea910d66039ac38cf6fe87d1fd
Helen Du <helendu.614@gmail.com>
2018-05-18 02:03:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,13 @@
 class Pokemon
+
+  attr_accesor :id, :name, :type, :db
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
 end
